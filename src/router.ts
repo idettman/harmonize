0dbedd64f13907a27dbf2f181dc5144b40eca65b
Kahler <212446691@logon.ds.ge.com>
2016-11-13 23:43:45 -0500

    router WIP

--- src/router.ts
+++ src/router.ts
@@ -99,13 +99,21 @@ export default ({routes, view}: {
         throw `What do you mean by this? ${JSON.stringify(route)}`
     };
 
-    // resolve all the routes to either a leaf or branch
-    const resolvedRoutes = (Object
-        .keys(routes)
-        .map(key => ({key, resolvedRoute: resolve(key, routes[key])}))
-    );
+    const rootPath = `/`;
+    const thisBranch: Branch = {
+        view,
+        path: rootPath,
+        routes: (Object
+            .keys(routes)
+            .map(key => ({key, resolvedRoute: resolve(key, routes[key])}))
+            .reduce((obj, {key, resolvedRoute}) => {
+                obj[key] = resolvedRoute;
+                return obj;
+            }, {} as {[key: string]: Leaf | Branch})
+        )
+    };
 
-    console.log(`resolved routes`, resolvedRoutes);
+    console.log(`this branch`, thisBranch);
 
     /**
      * takes in a leaf or a branch and returns a list of all the contains inside
@@ -116,23 +124,23 @@ export default ({routes, view}: {
     }[] {
         if ((route as Leaf).container) {
             return [{
-                path: route.path,
+                path: `${path}`,
                 container: (route as Leaf).container
             }];
         }
 
         const routes = (route as Branch).routes;
         const nestedContainerList = (Object
-            .keys(routes)
-            .map(key => ({
-                path: routes[key].path,
-                containers: flatMapRoute(`${path}/${routes[key].path}`, routes[key])
+            .keys (routes)
+            .map (key => ({
+                containers: flatMapRoute (
+                    `${path}/${routes[key].path}`,
+                    routes[key]
+                )
             }))
-            .reduce(
-                (obj, {path, containers}) => {
-
-                    
-                    return [...obj, ...containers]
+            .reduce (
+                (obj, {containers}) => {
+                    return [...obj, ...containers];
                 },
                 [] as {path: string, container: Container<any>}[]
             )
@@ -141,66 +149,92 @@ export default ({routes, view}: {
         return nestedContainerList;
     }
 
-    
-    const nestedContainers = (resolvedRoutes
-        .map(({key, resolvedRoute}) => ({
-            key,
-            routeList: flatMapRoute(key, resolvedRoute)
-        }))
-        .reduce(
-            (obj, {key, routeList}) => {
-                const reducedRoutes = routeList.reduce(
-                    (obj, {path, container}) => {
-                        obj[path] = container;
-                        return obj;
-                    },
-                    {} as {[key: string]: Container<any>}
-                );
-                return Object.assign({}, obj, reducedRoutes);
-            },
-            {} as {[key: string]: Container<any>}
-        )
+    const nestedContainers = flatMapRoute(``, thisBranch).reduce(
+        (obj, {path, container}) => {
+            obj[path] = container;
+            return obj;
+        },
+        {} as {[key: string]: Container<any>}
     );
-    console.log(`nestedContainers`, nestedContainers);
+
+    console.log (`flatMapRoute`, nestedContainers);
     
 
     const routerContainer = container ({
         nestedContainers,
         initialState: [] as string[],
-        view: ({model: path, containers}) => {
+        view: ({model: path, containers, props, update}) => {
 
             let placeHolder: any;
 
-            function resolveView (route: Leaf | Branch, path: string[], key: string): (props?: any) => VNode {
-                if (path.length == 1) {
-                    return containers[key];
+            function resolveView (
+                route: Leaf | Branch,
+                path: string[],
+                key: string
+            ): (props?: any) => VNode {
+                console.log (`path`, path);
+                if ((route as Leaf).container) {
+                    console.log(`leaf`, containers[key]);
+                    console.log(`containers`, containers);
+                    return containers[`/${key}`];
                 }
 
-                const currentPage = resolveView (route[path[0]], path.slice(1), ``);
-
                 const branch = route as Branch;
+                console.log(`branch`, branch);
+                const firstRoute = branch.routes[Object.keys(branch.routes)[0]];
+                const currentPage = resolveView (
+                    branch.routes[path[0]] || firstRoute,
+                    path.slice(1),
+                    /*if*/ key ? `${key}/${
+                        /*if*/ path[0] ? path[0] : firstRoute.path
+                    }` : firstRoute.path
+                );
+
+                const anchors = (Object
+                    .keys(branch.routes)
+                    .map(branchKey => ({
+                        branchKey,
+                        route: branch.routes[branchKey]
+                    }))
+                    .map(({branchKey, route}) => ({
+                        href: key ? `${key}/${branchKey}`: branchKey,
+                        title: (route as Leaf).title || route.path
+                    }))
+                    .map(({href, title}) => h('a', {
+                        props: {href},
+                        on: {click: update({
+                            by: (model, event) => {
+                                console.log(`clicked`, event);
+                                event.preventDefault();
+                                console.log(`currentModel`, model);
+                                console.log(`href`, href);
+                                console.log(`new model`, href.split(`/`));
+                                return href.split(`/`);
+                            }
+                        })}
+                    }, [title]))
+                );
 
                 let anchorList: any;
 
                 return (props?: any) => branch.view({
-                    anchorList: placeHolder,
+                    anchorList: anchors,
                     anchors: placeHolder,
-                    currentPage: currentPage
+                    currentPage
                 });
             }
 
-            const views = resolvedRoutes.map(({key, resolvedRoute}) => resolveView(
-                resolvedRoute,
-                [],
-                key
-            ));
+            const resolvedView = resolveView(thisBranch, path, ``);
+            console.log(`resolvedView`, resolvedView());
 
-            return view({
-                anchorList: placeHolder,
-                anchors: placeHolder,
-                currentPage: views[0]
-            });
+            return resolvedView(props);
         }
+    });
+
+    routerContainer.state$.addListener({
+        next: path => history.pushState(0, ``, path.join(`/`)),
+        error: error => console.log(error),
+        complete: () => console.log(`complete`)
     });
 
     return routerContainer;
